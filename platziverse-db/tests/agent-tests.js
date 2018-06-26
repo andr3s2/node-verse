e14392c09c5468b06ccd45ba2a5e4ac76ffe272b
Andrés Pineda <andr3s2@gmail.com>
2018-06-26 11:30:20 -0500

    Added tests part I

---
+++ platziverse-db/tests/agent-tests.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const test = require('ava')
+
+let db = null
+
+test.beforeEach(async () => {
+  const setupDatabase = require('../')
+  db = await  setupDatabase(config)
+})
+
+test('Agent', t => {
+  t.truthy(db.Agent, 'Agent service should exists')
+})
